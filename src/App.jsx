4d700a0c18ce90bc98d8a58c0fc56b09ad2f4551
Prioti Saha <tonnysaha15@gmail.com>
2023-10-15 23:31:52 +0600

    complete

---
+++ src/App.jsx
@@ -0,0 +1,20 @@
+import { useState } from 'react'
+import './App.css'
+import Header from './Header'
+import SideBar from './SideBar'
+import Home from './Home'
+
+function App() {
+
+
+  return (
+    <div className='grid-container'>
+      <Header/>
+      <SideBar/>
+      <Home/>
+
+    </div>
+  )
+}
+
+export default App
